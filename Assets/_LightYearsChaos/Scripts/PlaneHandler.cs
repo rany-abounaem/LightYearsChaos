35e322b5a0a75e3cb7c8d3fdaa8fc09f76a336eb
Rany Abounaem <ranyabounaem@gmail.com>
2023-07-19 19:30:24 +0300

    Added assets

---
+++ Assets/_LightYearsChaos/Scripts/PlaneHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+namespace LightYearsChaos
+{
+    public class PlaneHandler : MonoBehaviour
+    {
+        private void OnMouseDown()
+        {
+            Debug.Log("HELP");
+        }
+    }
+}
+
---
+++ Assets/_LightYearsChaos/Scripts/Unit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LightYearsChaos
+{
+    public class Unit : MonoBehaviour
+    {
+        private bool isSelected;
+
+        public bool IsSelected { get { return isSelected; } }
+    }
+}
